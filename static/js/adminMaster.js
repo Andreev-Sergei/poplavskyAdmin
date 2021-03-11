4ac351760201a18e1c63b610db116996843f24c3
Сергей Андреев <59507440+andeev-sergey@users.noreply.github.com>
2021-03-11 16:16:35 +0300

    edit

--- static/js/adminMaster.js
+++ static/js/adminMaster.js
@@ -1,1 +1,15 @@
- console.log('Success-2')
+function previewFile() {
+    var preview = document.getElementById('previewOutput');
+    var file = document.querySelector('input[type=file]').files[0];
+    var reader = new FileReader();
+
+    reader.onloadend = function() {
+        preview.src = reader.result;
+    }
+
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        preview.src = "";
+    }
+}
