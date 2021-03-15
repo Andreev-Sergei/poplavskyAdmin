efa8dca79dfd8270dfc36942794baa0f99dd2cd6
Сергей Андреев <59507440+andeev-sergey@users.noreply.github.com>
2021-03-15 16:12:24 +0300

    day-2

--- static/js/adminMaster.js
+++ static/js/adminMaster.js
@@ -13,3 +13,49 @@ function previewFile() {
         preview.src = "";
     }
 }
+
+modal = (bool) => {
+    if (bool) {
+        document.querySelector('body').classList.add('freeze')
+        document.querySelector('html').classList.add('freeze')
+        document.getElementById('modal').classList.add('active')
+        document.getElementById('overlay').classList.add('active')
+    } else {
+        document.querySelector('body').classList.remove('freeze')
+        document.querySelector('html').classList.remove('freeze')
+        document.getElementById('modal').classList.remove('active')
+        document.getElementById('overlay').classList.remove('active')
+    }
+}
+
+
+document.querySelectorAll('#requests article .request').forEach(btn => {
+    btn.addEventListener('click', () => {
+        modal(true)
+    })
+})
+document.getElementById('overlay').addEventListener('click', () => {
+    modal(false)
+})
+
+document.onkeydown = function(evt) {
+    evt = evt || window.event
+    var isEscape = false
+    if ("key" in evt) {
+        isEscape = (evt.key === "Escape" || evt.key === "Esc")
+    } else {
+        isEscape = (evt.keyCode === 27)
+    }
+    if (isEscape) {
+        modal(false)
+    }
+}
+
+document.querySelectorAll('#requests .tabs .tab').forEach(btn => {
+    btn.addEventListener('click', (event) => {
+        document.querySelector('#requests .tabs .tab.active').classList.remove('active')
+        event.target.classList.add('active')
+        let sortVariable = event.target.dataset.type
+        alert(sortVariable)
+    })
+})
